0642329d7992b501ee2b96e3050c8f0559e143b6
George Williams <williamsgeorge950@gmail.com>
2025-01-31 01:06:43 +0000

    Updated

--- variables.tf
+++ variables.tf
@@ -1,5 +1,16 @@
 variable "aws_region" {
-  description = "The AWS region where the infrastructure will be deployed"
+  description = "AWS region to deploy the resources"
   type        = string
   default     = "us-east-1"
+}
+
+variable "s3_bucket_name" {
+  description = "Name of the S3 bucket"
+  type        = string
+}
+
+variable "lambda_name" {
+  description = "Name of the Lambda function"
+  type        = string
+  default     = "lambda_translate"
 }

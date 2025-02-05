ef9e63a3e1f79652fdbde316adc7ce64aaaa415c
George Williams <williamsgeorge950@gmail.com>
2025-02-05 22:32:06 +0000

    Updated the root directory variable file with defaults

--- variables.tf
+++ variables.tf
@@ -4,9 +4,17 @@ variable "aws_region" {
   default     = "us-east-1"
 }
 
-variable "s3_bucket_name" {
-  description = "Name of the S3 bucket"
+# S3 Buckets for Requests and Responses
+variable "requests_bucket_name" {
+  description = "S3 bucket name for storing translation requests"
+  default = "requests_bucket_007"
   type        = string
+}
+
+variable "responses_bucket_name" {
+  description = "S3 bucket name for storing translation responses"
+  type        = string
+  default     = "response_bucket_007"  
 }
 
 variable "lambda_name" {

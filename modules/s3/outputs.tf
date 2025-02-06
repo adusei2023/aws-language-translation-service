6cda6fec17344ab491ad87fee1a0aac9f3217c22
George Williams <williamsgeorge950@gmail.com>
2025-02-06 16:00:30 +0000

    used the correct output names from your S3 module

--- modules/s3/outputs.tf
+++ modules/s3/outputs.tf
@@ -1,11 +1,23 @@
 # Output the name of the translation requests bucket
-output "requests_bucket_name" {
+output "s3_requests_bucket" {
   description = "Name of the translation requests S3 bucket"
   value       = aws_s3_bucket.requests_bucket.id
 }
 
 # Output the name of the translation responses bucket
-output "responses_bucket_name" {
+output "s3_responses_bucket" {
   description = "Name of the translation responses S3 bucket"
   value       = aws_s3_bucket.responses_bucket.id
+}
+
+# Output the ARN of the translation requests bucket
+output "s3_requests_bucket_arn" {
+  description = "ARN of the translation requests S3 bucket"
+  value       = aws_s3_bucket.requests_bucket.arn
+}
+
+# Output the ARN of the translation responses bucket
+output "s3_responses_bucket_arn" {
+  description = "ARN of the translation responses S3 bucket"
+  value       = aws_s3_bucket.responses_bucket.arn
 }

d42dc6c39435620fb49cd5eb07211cf3349ed058
George Williams <williamsgeorge950@gmail.com>
2025-02-06 15:57:28 +0000

    Passed both S3 bucket names to the Lambda function

--- main.tf
+++ main.tf
@@ -6,20 +6,24 @@ provider "aws" {
 # S3 Module - Creates an S3 bucket for storing translation input files
 module "s3" {
   source      = "./modules/s3"
-  bucket_name = var.s3_bucket_name
+  requests_bucket_name = var.s3_requests_bucket
+  responses_bucket_name = var.s3_responses_bucket
 }
 
 # IAM Module - Creates an IAM role for Lambda execution
 module "iam" {
   source = "./modules/iam"
+  requests_bucket_arn = module.s3.s3_requests_bucket_arn
+  responses_bucket_arn = module.s3.s3_responses_bucket_arn
 }
 
 # Lambda Module - Deploys the function that performs translations
 module "lambda" {
   source          = "./modules/lambda"
-  lambda_name     = var.lambda_name
+  lambda_function_name     = var.lambda_name
   lambda_role_arn = module.iam.lambda_role_arn
-  s3_bucket_name  = module.s3.bucket_name
+  s3_requests_bucket  = module.s3.s3_requests_bucket
+  s3_responses_bucket = module.s3.s3_responses_bucket
 }
 
 # API Gateway Module - Exposes the Lambda function via an HTTP endpoint

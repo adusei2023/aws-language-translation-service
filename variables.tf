62ec2f1477f366f5a359fdd25e15c35319d5d984
George Williams <williamsgeorge950@gmail.com>
2025-02-07 02:47:40 +0000

    updated root variables file to handle  errors

--- variables.tf
+++ variables.tf
@@ -7,14 +7,14 @@ variable "aws_region" {
 # S3 Buckets for Requests and Responses
 variable "s3_requests_bucket" {
   description = "S3 bucket name for storing translation requests"
-  default = "requests_bucket_007"
+  default     = "requests-bucket-007gw"
   type        = string
 }
 
 variable "s3_responses_bucket" {
   description = "S3 bucket name for storing translation responses"
   type        = string
-  default     = "response_bucket_007"  
+  default     = "response-bucket-007gw"
 }
 
 variable "lambda_name" {

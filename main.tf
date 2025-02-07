893392dbce946b0993e8724438b0f4e27ebd3d5f
George Williams <williamsgeorge950@gmail.com>
2025-02-07 02:41:57 +0000

    updated main.tf file

--- main.tf
+++ main.tf
@@ -1,33 +1,33 @@
 # Provider Configuration
 #provider "aws" {
- # region = var.aws_region
+# region = var.aws_region
 #}
 
 # S3 Module - Creates an S3 bucket for storing translation input files
 module "s3" {
-  source      = "./modules/s3"
-  requests_bucket_name = var.s3_requests_bucket
+  source                = "./modules/s3"
+  requests_bucket_name  = var.s3_requests_bucket
   responses_bucket_name = var.s3_responses_bucket
 }
 
 # IAM Module - Creates an IAM role for Lambda execution
 module "iam" {
-  source = "./modules/iam"
-  requests_bucket_arn = module.s3.s3_requests_bucket_arn
+  source               = "./modules/iam"
+  requests_bucket_arn  = module.s3.s3_requests_bucket_arn
   responses_bucket_arn = module.s3.s3_responses_bucket_arn
 }
 
 # Lambda Module - Deploys the function that performs translations
 module "lambda" {
-  source          = "./modules/lambda"
-  lambda_function_name     = var.lambda_name
-  lambda_role_arn = module.iam.lambda_role_arn
-  s3_requests_bucket  = module.s3.s3_requests_bucket
-  s3_responses_bucket = module.s3.s3_responses_bucket
+  source               = "./modules/lambda"
+  lambda_function_name = var.lambda_name
+  lambda_role_arn      = module.iam.lambda_role_arn
+  s3_requests_bucket   = module.s3.s3_requests_bucket
+  s3_responses_bucket  = module.s3.s3_responses_bucket
 }
 
 # API Gateway Module - Exposes the Lambda function via an HTTP endpoint
 module "api_gateway" {
-  source           = "./modules/api_gateway"
+  source            = "./modules/api_gateway"
   lambda_invoke_arn = module.lambda.lambda_invoke_arn
 }

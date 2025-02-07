46d0dfc1823d1b9e648bd35f6aefbba941724a15
George Williams <williamsgeorge950@gmail.com>
2025-02-07 02:45:23 +0000

    updated root output file

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,15 @@
-output "s3_bucket_name" {
-  description = "Name of the S3 bucket created"
-  value       = module.s3.bucket_name
+output "requests_bucket_name" {
+  value = module.s3.s3_requests_bucket
 }
+
+output "responses_bucket_name" {
+  value = module.s3.s3_responses_bucket
+}
+
 
 output "lambda_function_name" {
   description = "Lambda function name"
-  value       = module.lambda.lambda_name
+  value       = module.lambda.lambda_function_name
 }
 
 output "api_gateway_url" {

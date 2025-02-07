f822f17ef1ac68f9ec9bf91c54d1633fe27e5d35
George Williams <williamsgeorge950@gmail.com>
2025-02-07 02:44:30 +0000

    updated lambda output file

--- modules/lambda/outputs.tf
+++ modules/lambda/outputs.tf
@@ -7,3 +7,8 @@ output "lambda_function_name" {
   description = "The name of the Lambda function"
   value       = aws_lambda_function.translate_lambda.function_name
 }
+
+output "lambda_invoke_arn" {
+  description = "Invoke ARN of the Lambda function"
+  value       = aws_lambda_function.translate_lambda.invoke_arn
+}

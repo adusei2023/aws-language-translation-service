345ad5d2246b97efc035a9ca44fed77d089ff470
George Williams <williamsgeorge950@gmail.com>
2025-02-13 09:50:10 +0000

    updated api gateway module outputs.tf file

--- modules/api_gateway/outputs.tf
+++ modules/api_gateway/outputs.tf
@@ -1,11 +1,11 @@
 # Output API Gateway ID
 output "api_gateway_id" {
   description = "The ID of the API Gateway"
-  value       = aws_api_gateway_rest_api.translate_api.id
+  value       = aws_apigatewayv2_api.api_gateway.id
 }
 
 # Output API Gateway Endpoint URL
-output "api_gateway_url" {
-  description = "The invoke URL of the API Gateway"
-  value       = aws_api_gateway_deployment.translate_deployment.invoke_url
+output "api_gateway_endpoint" {
+  description = "Base URL for the API Gateway"
+  value       = aws_apigatewayv2_stage.api_stage.invoke_url
 }

629e1f8b83dc933782eb672d775bb70d2f0797c8
George Williams <williamsgeorge950@gmail.com>
2025-02-13 09:51:16 +0000

    updated api gateway module variables.tf file

--- modules/api_gateway/variables.tf
+++ modules/api_gateway/variables.tf
@@ -1,18 +1,42 @@
-# API Gateway Variables
-
-variable "api_name" {
-  description = "The name of the API Gateway"
-  type        = string
-  default     = "TranslateAPI"
+# Project Tags
+variable "tags" {
+  description = "Additional tags for the resource"
+  type        = map(string)
 }
 
-variable "api_description" {
-  description = "A brief description of the API"
+# Project Name
+variable "project" {
+  description = "Project name"
   type        = string
-  default     = "API Gateway for the translation service"
+  default     = "aws-lambda-translate"
 }
 
-variable "lambda_invoke_arn" {
-  description = "The invoke ARN of the Lambda function"
+# Deployment Environment
+variable "environment" {
+  description = "Environment name (e.g., dev, prod)"
+  type        = string
+}
+
+# AWS Region
+variable "region" {
+  description = "AWS region to deploy the resources"
+  type        = string
+}
+
+# Lambda Function ARN (For API Gateway Integration)
+variable "lambda_function_invoke_arn" {
+  description = "ARN of the Lambda function to invoke"
+  type        = string
+}
+
+# Lambda Function Name (For Permission Attachment)
+variable "lambda_function_name" {
+  description = "Name of the Lambda function"
+  type        = string
+}
+
+# KMS Key ID for Encryption
+variable "kms_key_id" {
+  description = "The KMS key ID to use for encrypting CloudWatch logs"
   type        = string
 }

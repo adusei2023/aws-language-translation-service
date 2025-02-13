9864d16b3e5842709038af651471d5792aa85534
George Williams <williamsgeorge950@gmail.com>
2025-02-13 20:52:08 +0000

    updated

--- modules/s3/outputs.tf
+++ modules/s3/outputs.tf
@@ -1,23 +1,11 @@
-# Output the name of the translation requests bucket
-output "s3_requests_bucket" {
-  description = "Name of the translation requests S3 bucket"
-  value       = aws_s3_bucket.requests_bucket.id
+# Output the S3 bucket name
+output "s3_translation_bucket_name" {
+  description = "Name of the S3 translation bucket"
+  value       = aws_s3_bucket.translation_bucket.id
 }
 
-# Output the name of the translation responses bucket
-output "s3_responses_bucket" {
-  description = "Name of the translation responses S3 bucket"
-  value       = aws_s3_bucket.responses_bucket.id
-}
-
-# Output the ARN of the translation requests bucket
-output "s3_requests_bucket_arn" {
-  description = "ARN of the translation requests S3 bucket"
-  value       = aws_s3_bucket.requests_bucket.arn
-}
-
-# Output the ARN of the translation responses bucket
-output "s3_responses_bucket_arn" {
-  description = "ARN of the translation responses S3 bucket"
-  value       = aws_s3_bucket.responses_bucket.arn
+# Output the S3 bucket ARN
+output "s3_translation_bucket_arn" {
+  description = "ARN of the S3 translation bucket"
+  value       = aws_s3_bucket.translation_bucket.arn
 }

2126369b36ed961b02a579f528afe6f5f3fe6cab
George Williams <williamsgeorge950@gmail.com>
2025-02-13 20:52:49 +0000

    updated

--- modules/s3/variables.tf
+++ modules/s3/variables.tf
@@ -1,11 +1,34 @@
-# Variable for the translation requests bucket name
-variable "requests_bucket_name" {
-  description = "S3 bucket name for storing translation requests"
+variable "bucket_name" {
+  description = "The name of the S3 bucket to create for translation data"
   type        = string
 }
 
-# Variable for the translation responses bucket name
-variable "responses_bucket_name" {
-  description = "S3 bucket name for storing translation responses"
+variable "kms_key_id" {
+  description = "The KMS key ID used for encrypting objects in the S3 bucket"
   type        = string
+}
+
+variable "tags" {
+  description = "Additional metadata tags for the S3 bucket"
+  type        = map(string)
+}
+
+variable "project" {
+  description = "The project name associated with this S3 bucket"
+  type        = string
+}
+
+variable "environment" {
+  description = "The deployment environment (e.g., dev, prod, staging)"
+  type        = string
+}
+
+variable "region" {
+  description = "AWS region where the S3 bucket will be deployed"
+  type        = string
+}
+
+variable "bucket_policy_actions" {
+  description = "List of S3 actions allowed in the bucket policy"
+  type        = list(string)
 }

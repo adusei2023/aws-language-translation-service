8f6dd1dbbc8f41dff6e0b9501e0c26ea12c887ab
George Williams <williamsgeorge950@gmail.com>
2025-02-13 20:45:17 +0000

    updated

--- outputs.tf
+++ outputs.tf
@@ -1,18 +1,27 @@
-output "requests_bucket_name" {
-  value = module.s3.s3_requests_bucket
+# ===========================
+#   Terraform Outputs
+# ===========================
+
+# Output the name of the translation request bucket
+output "translation_request_bucket_name" {
+  description = "Name of the S3 bucket storing translation requests"
+  value       = module.translation_request_bucket.bucket_name
 }
 
-output "responses_bucket_name" {
-  value = module.s3.s3_responses_bucket
+# Output the name of the translation response bucket
+output "translation_response_bucket_name" {
+  description = "Name of the S3 bucket storing translation responses"
+  value       = module.translation_response_bucket.bucket_name
 }
 
-
-output "lambda_function_name" {
-  description = "Lambda function name"
-  value       = module.lambda.lambda_function_name
+# Output the deployed Lambda function name
+output "translation_lambda_function_name" {
+  description = "Name of the deployed AWS Lambda function"
+  value       = module.translation_lambda.function_name
 }
 
-output "api_gateway_url" {
-  description = "Base URL for API Gateway"
-  value       = module.api_gateway.api_gateway_url
+# Output the API Gateway base URL
+output "translation_api_gateway_url" {
+  description = "Base URL for the API Gateway endpoint"
+  value       = module.translation_api_gateway.api_gateway_endpoint
 }

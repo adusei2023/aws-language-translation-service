e9ad55d0e94552ffaa6309a302c731fd2564f57d
George Williams <williamsgeorge950@gmail.com>
2025-02-13 19:33:22 +0000

    updated

--- modules/lambda/outputs.tf
+++ modules/lambda/outputs.tf
@@ -1,14 +1,17 @@
-output "lambda_arn" {
+# Output the ARN of the Lambda function
+output "lambda_function_arn" {
   description = "The ARN of the created Lambda function"
-  value       = aws_lambda_function.translate_lambda.arn
+  value       = aws_lambda_function.translate_function.arn
 }
 
+# Output the function name for reference
 output "lambda_function_name" {
   description = "The name of the Lambda function"
-  value       = aws_lambda_function.translate_lambda.function_name
+  value       = aws_lambda_function.translate_function.function_name
 }
 
-output "lambda_invoke_arn" {
+# Output the function invocation ARN for API Gateway integration
+output "lambda_function_invoke_arn" {
   description = "Invoke ARN of the Lambda function"
-  value       = aws_lambda_function.translate_lambda.invoke_arn
+  value       = aws_lambda_function.translate_function.invoke_arn
 }

0f16eab4b43b3ac8d0a063d9cc8807e27c546f5c
adusei2023 <samuel.boateng01@yahoo.com>
2025-06-25 15:09:46 +0000

    Improved Descriptions

--- outputs.tf
+++ outputs.tf
@@ -1,23 +1,23 @@
 # Output the name of the S3 bucket where translation requests are stored
 output "request_bucket_name" {
-  description = "The name of the S3 bucket used for storing translation requests"
+  description = "The name of the S3 bucket that stores incoming translation requests before processing"
   value       = module.request_bucket.bucket_name
 }
 
 # Output the name of the S3 bucket where translation responses and logs are stored
 output "response_bucket_name" {
-  description = "The name of the S3 bucket used for storing translation responses"
+  description = "The name of the S3 bucket that stores completed translation results and processing logs"
   value       = module.response_bucket.bucket_name
 }
 
 # Output the name of the deployed AWS Lambda function
 output "lambda_function_name" {
-  description = "The name of the AWS Lambda function responsible for translation processing"
+  description = "The name of the AWS Lambda function that handles text translation operations using Amazon Translate"
   value       = module.lambda.function_name
 }
 
 # Output the API Gateway endpoint URL for making translation requests
 output "api_gateway_url" {
-  description = "The base URL of the API Gateway used to invoke the Lambda function"
+  description = "The public endpoint URL of the API Gateway that clients can use to submit translation requests"
   value       = module.api_gateway.api_gateway_endpoint
 }
